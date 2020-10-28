c7fdc16252cf499ff71c746dd8c156a08060eeef
Slava Lebedintsev <xcarvedx@yandex.ru>
2020-10-28 14:00:50 +0300

    fix: исправлен роут cards

--- controllers/card.js
+++ controllers/card.js
@@ -54,7 +54,7 @@ module.exports.likeCard = (req, res, next) => {
 };
 
 module.exports.dislikeCard = (req, res, next) => {
-  Card.findByIdAndUpdate(req.params._id,
+  Card.findByIdAndUpdate(req.params.cardId,
     { $pull: { likes: req.user._id } },
     { new: true })
     .orFail()
