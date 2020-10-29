e2aa1261f9b21ddf40d6f0baab1c84c0b299939a
Slava Lebedintsev <xcarvedx@yandex.ru>
2020-10-29 13:22:53 +0300

    fix: решение проблем password

--- middlewares/reqValidation.js
+++ middlewares/reqValidation.js
@@ -24,7 +24,7 @@ const validateId = celebrate({
 const validateUser = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required().alphanum().min(8),
   }),
 });
 
